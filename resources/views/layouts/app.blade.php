12f50ad48d5508842e5c8651556040b36a6874f4
zlEliasV <zleliasvillalba@gmail.com>
2025-06-29 17:21:21 -0300

    Cambio en el mensaje Ferreteria

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/">Ferretería App</a>
+            <a class="navbar-brand" href="/">Ferretería</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
